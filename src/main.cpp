a1c945169a9fc2cf1c9a232338dfe35ed5af215f
Volodymyr Shamray <shamray@outlook.com>
2017-03-04 00:22:51 +0200

    Investigate random distribution for age

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,53 @@
 #include "boost/date_time/gregorian/gregorian.hpp"
+
+#include <random>
 #include <iostream>
+
+using namespace boost::gregorian;
+using namespace std;
 
 struct person
 {
-  boost::gregorian::date birth_date;
+  date birth_date;
+  int life_expectancy_t;
 };
 
 struct environment
 {
+  month_iterator current = date{ 1991, Sep, 1 };
   double annual_birth_rate = 0;
+
+  struct life_expectancy_t {
+    normal_distribution<> male { 61.56, 15 };
+    normal_distribution<> female { 74.03, 2.0 };
+  };
+
+  life_expectancy_t life_expectancy;
+
+  void event_person_born()
+  {}
+
+  void event_person_died()
+  {}
 };
+
+auto generate_population(environment& env)
+{
+  constexpr auto size = 1000000;
+
+}
 
 int main()
 {
-  std::cout << "hello world";
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  auto env = environment {};
+  //cout << fixed;
+  cout.precision(4);
+  //for (environment env = {}; *env.current <= date{ 1993, Jan, 1 }; ++env.current)
+  for (auto i = 0; i < 1000; ++i)
+  {
+    cout << env.life_expectancy.male(gen) << endl;
+  }
 }
