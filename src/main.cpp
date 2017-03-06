e9cf976b9a8e07d9cb4b05baec98f5408f52ef3f
Volodymyr Shamray <shamray@outlook.com>
2017-03-06 23:02:51 +0200

    Implement event processing loop and life_expectancy_distribution

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include "boost/date_time/gregorian/gregorian.hpp"
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/range/iterator_range.hpp>
 
 #include <random>
+#include <list>
 #include <iostream>
 
 using namespace boost::gregorian;
@@ -26,7 +28,43 @@ namespace utils { namespace datetime
 
     assert(months >= 1 && months <= 12);
 
-    return date{ full_years, months, today.day() };
+    auto result_year = boost::numeric_cast<unsigned short>(today.year() - full_years);
+    auto result_month = boost::numeric_cast<unsigned short>(0);
+
+    if (months >= today.month())
+    {
+      result_month = months - today.month();
+      result_year--;
+    }
+    else
+    {
+      result_month = today.month() - months;
+    }
+
+    return date{ result_year, result_month, today.day() };
+  }
+
+  auto years_after(double years, date today)
+  {
+    const auto full_years = boost::numeric_cast<unsigned short>(ceil(years));
+    const auto months = boost::numeric_cast<unsigned short>(round((years - full_years) * 12));
+
+    assert(months >= 1 && months <= 12);
+
+    auto result_year = boost::numeric_cast<unsigned short>(today.year() + full_years);
+    auto result_month = boost::numeric_cast<unsigned short>(0);
+
+    if (months + today.month() >= 12)
+    {
+      result_month = months - today.month();
+      result_year++;
+    }
+    else
+    {
+      result_month = today.month() + months;
+    }
+
+    return date{ result_year, result_month, today.day() };
   }
 }}
 enum class gender_t
@@ -35,17 +73,19 @@ enum class gender_t
   female
 };
 
+struct person
+{
+  date birth_date;
+  gender_t gender;
+  int life_expectancy;
+};
+
 struct environment
 {
   month_iterator current = date{ 1991, Sep, 1 };
   double annual_birth_rate = 0;
-
-  struct life_expectancy_t {
-    normal_distribution<> male { 61.56, 15 };
-    normal_distribution<> female { 74.03, 2.0 };
-  };
-
-  life_expectancy_t life_expectancy;
+  list<person> population;
+  multimap<date, function<void()>> events;
 
   void event_person_born()
   {}
@@ -55,13 +95,6 @@ struct environment
 
   environment(environment&&) = default;
   environment& operator=(environment&&) = default;
-};
-
-struct person
-{
-  date birth_date;
-  gender_t gender;
-  int life_expectancy;
 };
 
 class population_distribution
@@ -107,23 +140,56 @@ private:
   bernoulli_distribution gender_distribution_;
 };
 
-auto generate_population(environment& env, const population_distribution& distribution)
+class life_expectancy_distribution
+{
+public:
+  life_expectancy_distribution(double male_mean, double female_mean)
+    : male_{ male_mean, 15 }
+    , female_{ female_mean, 15 }
+  {}
+
+  auto operator()(gender_t gender)
+  {
+    if (gender == gender_t::male)
+      return male_(utils::random::generator());
+    else
+      return female_(utils::random::generator());
+  }
+
+  auto operator()(date today, gender_t gender)
+  {
+    return utils::datetime::years_after(operator()(gender), today);
+  }
+
+private:
+  normal_distribution<> male_{ 61.56, 15 };
+  normal_distribution<> female_{ 74.03, 15 };
+};
+
+auto generate_population(environment& env, population_distribution& distribution)
 {
   constexpr auto size = 1000000;
-
+  for (auto i = 0; i < size; ++i)
+    env.population.push_back(distribution(*env.current));
 }
 
 int main()
 {
-  auto env = environment {};
-  //cout << fixed;
   cout.precision(4);
-  auto d = population_distribution{ {0, 30, 60, 70, 110}, {2, 2.5, 1.5, 1, 0}, 0.4 };
-  //for (environment env = {}; *env.current <= date{ 1993, Jan, 1 }; ++env.current)
-  for (auto i = 0; i < 1000; ++i)
-  {
-    cout << get<0>(d()) << endl;
-    //cout << env.life_expectancy.male(utils::random::generator()) << endl;
-  }
 
+  auto env = environment {};
+  auto pd = population_distribution{ {0, 30, 60, 70, 110}, {2, 2.5, 1.5, 1, 0}, 0.4 };
+  auto led = life_expectancy_distribution{ 61.56, 74.03 };
+
+  generate_population(env, pd);
+
+  for (; *env.current <= date{ 2005, Jan, 1 }; ++env.current)
+  {
+    for (auto&& event_pair : boost::make_iterator_range(env.events.begin(), env.events.upper_bound(*env.current)))
+    {
+      event_pair.second();
+    }
+
+    env.events.erase(env.events.begin(), env.events.upper_bound(*env.current));
+  }
 }
